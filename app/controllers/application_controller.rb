e0dbc2441cdd2489b962bb391e8acd8f2b5ebdb2
azheng249 <andy.zheng249@gmail.com>
2016-08-02 20:40:18 -0400

    added helpers to request authorization from hue bridge. added spec for #get_hue_authorization helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :hue_ip
+  helper_method :all
+  include ApplicationHelper
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -10,5 +10,17 @@ describe ApplicationHelper do
       expect(hue_ip).to be_a(String)
       expect(hue_ip.gsub(".", "").to_i).not_to be(0)
     end
+
+    it "should response with false if no ip address" do
+      expect(hue_ip).to be_a(String).or(eq(false))
+    end
+  end
+
+  describe "#get_hue_authorization" do
+    it "returns a hash with a success bool and a message" do
+      resp = get_hue_authorization
+      expect(resp[:success]).to eq(true).or(eq(false))
+      expect(resp[:message]).to be_truthy
+    end
   end
 end
